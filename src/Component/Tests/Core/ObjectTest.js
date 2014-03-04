08033aa9b3219e8646d265895051573456a67500
Mariano Pardo <marianomdq1@gmail.com>
2014-03-03 22:43:40 -0300

    Added new tests to rode.Object

--- src/Component/Tests/Core/ObjectTest.js
+++ src/Component/Tests/Core/ObjectTest.js
@@ -15,6 +15,36 @@ describe('rode.Object', function () {
     });
 
     /**
+     * Check if the initialize() method is called correctly
+     */
+    it('should call the initialize method of the new instances', function () {
+        var myObject = new rode.Object({
+            initialize: function () {
+                this.name = 'my name'
+            }
+        });
+        expect(myObject instanceof rode.Object).to.be(true);
+        expect(myObject.initialize).to.be.a('function');
+        expect(myObject.name).to.be('my name');
+    });
+
+    /**
+     * Check if two instances of rode.Object are isolated
+     */
+    it('should keep isolated two instances', function () {
+        var object1 = new rode.Object({
+            name: 'object 1'
+        });
+        var object2 = new rode.Object({
+            name: 'object 2'
+        });
+        expect(object1 instanceof rode.Object).to.be(true);
+        expect(object2 instanceof rode.Object).to.be(true);
+        expect(object1.name).to.be('object 1');
+        expect(object2.name).to.be('object 2');
+    });
+
+    /**
      * Check if Object can be extended
      */
     it('should be extensible', function () {
@@ -32,16 +62,28 @@ describe('rode.Object', function () {
     });
 
     /**
-     * Check if the initialize() method is called correctly
+     * Check if Object can be extended
      */
-    it('should call the initialize method of the new instances', function () {
-        var myObject = new rode.Object({
-            initialize: function () {
-                this.name = 'my name'
+    it('should be extensible in multiple levels', function () {
+        var MyClass = rode.Object.extend({
+            getName: function () {
+                return this.name
             }
         });
-        expect(myObject instanceof rode.Object).to.be(true);
-        expect(myObject.initialize).to.be.a('function');
-        expect(myObject.name).to.be('my name');
+        var MyChildClass = MyClass.extend({
+            initialize: function () {
+                this.status = 'ok';
+            },
+            getUpperName: function () {
+                return this.getName().toUpperCase();
+            }
+        });
+        var myInstance = new MyChildClass({
+            name: 'my name'
+        });
+        expect(myInstance instanceof MyClass).to.be(true);
+        expect(myInstance.getUpperName()).to.be('MY NAME');
+        expect(myInstance.status).to.be('ok');
+        expect(MyChildClass.extend).to.be.a('function');
     });
 });
