10f597053873fc7b4a0431c8efe27f488d50d875
Mariano Pardo <marianomdq1@gmail.com>
2014-03-28 18:28:23 -0300

    Implemented index and modules loader

---
+++ index.js
@@ -0,0 +1,18 @@
+// NOTE: This is the only file that does not support ES6 //
+
+var traceur = require('traceur');
+var path = require('path');
+require('es6-shim');
+require('es6-module-loader');
+
+// base path
+global.__rodeBase = __dirname;
+
+// ES6 files
+traceur.require.makeDefault(function(filename) {
+  // all the files outside the node_modules directory will use ES6
+  return filename.startsWith(__dirname) && !filename.startsWith(path.join(__dirname, 'node_modules'));
+});
+
+// export rode.js
+module.exports = require('./loader').rode;
---
+++ loader.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var fs = require('fs');
+import { Core } from './src/Core/Core';
+import { PackageList } from './src/Package/PackageList';
+import { Package } from './src/Package/Package';
+import { Router } from './src/Router/Router';
+import { List } from './src/Util/List';
+
+// Find the app root path
+var rootPath = path.resolve(__dirname, '../../');
+var packageJson = path.resolve(rootPath, 'package.json');
+if (!fs.existsSync(packageJson)) {
+  rootPath = __dirname;
+}
+
+var core = new Core(rootPath);
+core.packageList = new PackageList;
+
+// for singletons export the instance
+export var rode = core;
+export var packageList = core.packageList;
+
+// export all the public classes
+export {Package};
+export {Router};
+export {List};
